760db2bf69a389a70cf3e335f71d4e1e99ca82fe
zullin <fay@zull.in>
2018-07-03 16:40:42 +0300

    i18n: notification

--- src/components/Notification.vue
+++ src/components/Notification.vue
@@ -4,52 +4,50 @@
       <li class="list-item"
         :class="item.read ? 'read' : ''"
         @click="redirect(item)" 
-        v-for="item in allNotifications">
+        v-for="item in allNotifications"
+        :key="item.id">
         <div class="item-avatar">
           <fa-icon :icon="iconName(item.notification_type)" style="font-size:18px" />
         </div>
         <div class="item-main">
           <p v-if="item.notification_type === 1">
-            <b>{{item.data.display_username}}</b>
-            @你了在
+            <b>@{{item.data.display_username}}</b>
+            {{ $t('notification.mentioned') }}
             <router-link :to="`/topic/${item.topic_id}`">{{item.fancy_title}}</router-link>
-            中
           </p>
           <p v-if="item.notification_type === 2">
             <b>{{item.data.display_username}}</b>
-            回复了你
-            <a>{{item.fancy_title}}</a>
+            {{ $t('notification.replied') }}
+            <router-link :to="`/topic/${item.topic_id}`">{{item.fancy_title}}</router-link>
           </p>
           <p v-if="item.notification_type === 4">
             <b>{{item.data.display_username}}</b>
-            编辑了
+            {{ $t('notification.edited') }}
             <router-link :to="`/topic/${item.topic_id}`">{{item.fancy_title}}</router-link>
           </p>
           <p v-if="item.notification_type === 5">
             <b>{{item.data.display_username}}</b>
-            赞了你在
+            {{ $t('notification.liked') }}
             <router-link :to="`/topic/${item.topic_id}`">{{item.fancy_title}}</router-link>
-            中的回复
           </p>
           <p v-if="item.notification_type === 6">
             <b>{{item.data.display_username}}</b>
-            给你发送了消息
+            {{ $t('notification.private_message') }}
             <a>{{item.fancy_title}}</a>
           </p>
           <p v-if="item.notification_type === 9">
             <b>{{item.data.display_username}}</b>
-            评论了
+            {{ $t('notification.posted') }}
             <router-link :to="`/topic/${item.topic_id}`">{{item.fancy_title}}</router-link>
           </p>
           <p v-if="item.notification_type === 12">
-            <b>{{item.data.username}}</b>设置了{{item.data.badge_name}}
+            <b>{{item.data.username}}</b> {{ $t('notification.granted_badge') }} {{item.data.badge_name}}
           </p>
           <!-- for custom resolve plugin -->
           <p v-if="item.notification_type === 14 && item.data.message === 'solved.accepted_notification'">
             <b>{{item.data.display_username}}</b>
-            接受了你在
+            {{ $t('notification.accepted') }}
             <router-link :to="`/topic/${item.topic_id}`">{{item.fancy_title}}</router-link>
-            中的回复
           </p>
         </div>
         <div class="item-right">
@@ -66,49 +64,74 @@ import moment from 'moment'
 import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
 
 export default {
-  name: 'Notification',
-  computed: {
-    ...mapGetters({
-      allNotifications: 'Notification/all'
-    }),
-  },
-  components: {
-    'fa-icon': FontAwesomeIcon,
-  },
-  methods: {
-    //todo: switch
-    iconName(type) {
-      if (type === 1) {
-        return ['fas', 'at']
-      } else if (type === 2) {
-        return 'reply'
-      } else if (type == 4) {
-        return ['far', 'edit']
-      } else if (type === 5) {
-        return ['far', 'heart']
-      } else if (type === 6) {
-        return ['far', 'envelope']
-      } else if (type === 9) {
-        return ['far', 'comment']
-      } else if (type === 12) {
-        return ['far', 'heart']
-      } else if (type === 14) {
-        return ['far', 'check-circle']
-      }
+    name: 'Notification',
+    computed: {
+        ...mapGetters({
+            allNotifications: 'Notification/all'
+        }),
     },
-    formatDate(d) {
-      return moment(d).fromNow()
+
+    components: {
+        'fa-icon': FontAwesomeIcon,
     },
-    redirect(item) {
-      this.$router.push({
-        path: `/topic/${item.topic_id}#${item.post_number - 1}`,
-      })
+
+    methods: {
+        iconName(type) {
+            let result = []
+
+            switch(type) {
+                case 1:
+                    result = ['fas', 'at']
+                    break
+
+                case 2:
+                    result = 'reply'
+                    break
+
+                case 4:
+                    result = ['far', 'edit']
+                    break
+
+                case 5:
+                    result = ['far', 'heart']
+                    break
+
+                case 6:
+                    result = ['far', 'envelope']
+                    break
+
+                case 9:
+                    result = ['far', 'comment']
+                    break
+
+                case 12:
+                    result = ['far', 'heart']
+                    break
+
+                case 14:
+                    result = ['far', 'check-circle']
+                    break
+            }
+
+            return result
+        },
+
+        formatDate(d) {
+            return moment(d).fromNow()
+        },
+        
+        redirect(item) {
+            this.$router.push({
+                path: `/topic/${item.topic_id}#${item.post_number - 1}`,
+            })
+        },
     },
-  },
-  mounted() {
-    this.$scrollTo('body', 300)
-  },
+
+    mounted() {
+        this.$scrollTo('body', 300)
+    },
 }
+
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
