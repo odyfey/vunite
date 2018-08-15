67ac6dc18ce01eecf44235aa4179da935788ad86
zullin <fay@zull.in>
2018-08-15 16:47:14 +0300

    changes in discourse api after upgrade to 2.1

--- src/store/modules/User.js
+++ src/store/modules/User.js
@@ -1,14 +1,14 @@
-import Vue from 'vue'
-import { config } from '@/config'
-import { TOKEN_STORAGE, API_KEY_STORAGE, USERINFO_STORAGE } from '@/const'
+import Vue from "vue"
+import { config } from "@/config"
+import { TOKEN_STORAGE, API_KEY_STORAGE, USERINFO_STORAGE } from "@/const"
 
 const namespaced = true
 
 const state = {
-    bearer: '',
-    apiKey: '',
+    bearer: "",
+    apiKey: "",
     userInfo: {},
-    error: ''
+    error: ""
 }
 
 const getters = {
@@ -18,75 +18,91 @@ const getters = {
 
     authorized: state => !!state.bearer,
 
-    username: state => ( state.userInfo ? state.userInfo.name : '' ),
+    username: state => (state.userInfo ? state.userInfo.name : "")
 }
 
 const mutations = {
-    setBearer: (state, bearer) => state.bearer = bearer,
+    setBearer: (state, bearer) => (state.bearer = bearer),
 
-    setApiKey: (state, key) => state.apiKey = key,
+    setApiKey: (state, key) => (state.apiKey = key),
 
     // info: { id, name }
-    setUserInfo: (state, info) => state.userInfo = info,
+    setUserInfo: (state, info) => (state.userInfo = info),
 
-    setError: (state, error) => state.error = error,
+    setError: (state, error) => (state.error = error),
 
     clear(state) {
-        state.bearer = ''
-        state.apiKey = ''
+        state.bearer = ""
+        state.apiKey = ""
         state.userInfo = {}
-        state.error = ''
-    },
+        state.error = ""
+    }
 }
 
 const actions = {
     async generateApiKey({ commit, state }) {
         try {
-            const { data } = await Vue.http.get(`${config.discourse.backend}/admin/users/${state.userInfo.id}/generate_api_key?api_key=${config.discourse.apiKey}&api_username=${state.userInfo.name}`)
+            const form = new FormData()
+            form.append("api_key", config.discourse.apiKey)
+            form.append("api_username", state.userInfo.name)
 
-            commit('setApiKey', data.api_key.key)
+            const { data } = await Vue.http.post(
+                `${config.discourse.backend}/admin/users/${
+                    state.userInfo.id
+                }/generate_api_key`,
+                form,
+                {
+                    "content-type": `multipart/form-data; boundary=${
+                        form._boundary
+                    }`
+                }
+            )
+
+            commit("setApiKey", data.api_key.key)
             localStorage.setItem(API_KEY_STORAGE, data.api_key.key)
-        }
-        catch (error) {
-            commit('setError', error.data)
+        } catch (error) {
+            commit("setError", error.data)
             localStorage.removeItem(API_KEY_STORAGE)
         }
     },
 
     async fetchUser({ commit, dispatch }, username) {
         try {
-            const response = await Vue.http.get(`/users/${username}.json`)
+            const { data } = await Vue.http.get(`/users/${username}.json`)
 
-            const user = response.data.users[0]
+            const user = data.user
             const info = {
                 id: user.id,
                 name: user.username
             }
 
-            commit('setUserInfo', info)
+            commit("setUserInfo", info)
             localStorage.setItem(USERINFO_STORAGE, JSON.stringify(info))
 
-            dispatch('generateApiKey')
-        }
-        catch (error) {
-            commit('setError', error.data)
+            dispatch("generateApiKey")
+        } catch (error) {
+            commit("setError", error.data)
             localStorage.removeItem(USERINFO_STORAGE)
         }
     },
 
     async fetchBearerToken({ commit, dispatch }, query) {
         try {
-            const { data } = await Vue.http.get(`${config.discourse.ssoProxy}/getToken?sso=${query.sso}&sig=${query.sig}`, {
-                responseType: 'text'
-            })
+            const { data } = await Vue.http.get(
+                `${config.discourse.ssoProxy}/getToken?sso=${query.sso}&sig=${
+                    query.sig
+                }`,
+                {
+                    responseType: "text"
+                }
+            )
 
-            commit('setBearer', data.token)
+            commit("setBearer", data.token)
             localStorage.setItem(TOKEN_STORAGE, data.token)
 
-            dispatch('fetchUser', data.username)
-        }
-        catch (error) {
-            commit('setError', error.data)
+            dispatch("fetchUser", data.username)
+        } catch (error) {
+            commit("setError", error.data)
             localStorage.removeItem(TOKEN_STORAGE)
         }
     }
@@ -97,5 +113,5 @@ export default {
     state,
     getters,
     mutations,
-    actions,
+    actions
 }
